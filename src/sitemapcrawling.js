26b8dc633cba90ef7637c4f5c761d3b83e834ad6
sachinWebsoham <142967198+sachinWebsoham@users.noreply.github.com>
2024-03-05 18:56:32 +0530

    intial commit

--- src/sitemapcrawling.js
+++ src/sitemapcrawling.js
@@ -2,12 +2,7 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const urlParser = require("url");
 const { supabase } = require("../config/config");
-const {
-  validDomain,
-  getUrl,
-  ignoreExtention,
-  getRandomUserAgent,
-} = require("../utils/index.js");
+const { validDomain, getRandomUserAgent } = require("../utils/index.js");
 
 const headers = {
   "User-Agent": getRandomUserAgent(),
@@ -20,222 +15,168 @@ const headers = {
   "Cache-Control": "max-age=0",
 };
 
-// for update external link in supabase
-let externalLink = [];
-const externalLinkAdd = async () => {
-  if (externalLink?.length > 0) {
-    const { data } = await supabase
-      .from("crawling_external_link")
-      .upsert(externalLink, { onConflict: ["url"] });
-    externalLink = [];
-    console.log(data, "externalLink");
-  } else {
-    return;
-  }
-};
 const seenUrls = {};
 const seenExternalLink = {};
-
-// for update internal link in supabase
-let pages = [];
-let internalLinks = [];
-const internalLinkAdd = async () => {
-  if (internalLinks?.length > 0) {
-    const { data } = await supabase
-      .from("crawling_internal_link")
-      .upsert(internalLinks, { onConflict: ["page_url"] });
-    internalLinks = [];
-    console.log(data, "internal error link");
-  } else {
-    return;
-  }
-};
-
-// for update error pages in supabase
-let internalLinkError = [];
-const errorInternalLinkAdd = async () => {
-  if (internalLinkError?.length > 0) {
-    const { data } = await supabase
-      .from("crawling_internal_link")
-      .upsert(internalLinkError, { onConflict: ["page_url"] });
-    internalLinkError = [];
-    console.log(data, "internal error link");
-  } else {
-    return;
-  }
-};
-// let sitemapInternalLink = [];
-// const sitemapInternalLinkAdd = async () => {
-//   if (sitemapInternalLink?.length > 0) {
-//     const { data } = await supabase
-//       .from("crawling_internal_link")
-//       .upsert(sitemapInternalLink, { onConflict: ["page_url"] });
-//     sitemapInternalLink = [];
-//     console.log(data, "sitemap internal link");
-//   } else {
-//     return;
-//   }
-// };
-
-let sitemapCount = 1;
-const extractSitemap = async (currentUrl) => {
-  let sitemap = [];
-  try {
-    const response = await axios.get(currentUrl);
-    const $ = cheerio.load(response.data, { xmlMode: true });
-
-    const xmlPages = $("loc")
-      .map((i, loc) => $(loc).text())
-      .get();
-    if (xmlPages?.length > 0) {
-      sitemap.push(...xmlPages);
-      for (const xmlPage of xmlPages) {
-        try {
-          await extractSitemap(xmlPage);
-        } catch (error) {
-          console.log(error.message);
-        }
-      }
-    } else {
-      crawl({
-        url: currentUrl,
-        ignore: "/search",
-      });
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
-  return sitemap;
-};
-
 let counter = 1;
 const crawl = async ({ url, ignore }) => {
   try {
     if (seenUrls[url]) return;
     seenUrls[url] = true;
     const { host, protocol } = urlParser.parse(url);
+    // console.log(url, ">>>>>>>>>");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await axios.get(url, headers);
     const contentType = response.headers["content-type"];
 
     if (!contentType.includes("text/html")) return;
     const $ = cheerio.load(response.data);
 
-    // crawl all a tags
     const links = $("a")
       .map((i, link) => link.attribs.href)
       .get();
     if (seenUrls[url]) {
-      await supabase.from("crawling_internal_link").upsert(
-        { page_url: url, status: true },
-        {
-          onConflict: ["page_url"],
-          updateColumns: ["status"],
-        }
-      );
+      try {
+        await supabase.from("sitemap_internal_link").upsert(
+          { page_url: url, status: true, message: "done" },
+          {
+            onConflict: ["page_url"],
+            updateColumns: ["status", "message"],
+          }
+        );
+      } catch (error) {
+        console.log(error.message, "internal link update");
+      }
+
+      // console.log(error, "update internal link");
     }
-    console.log(`Crawling At Page no.: ${counter++}\n____________________`);
+    console.log(`Crawling At Page no.: ${counter++}`);
 
     if (links?.length > 0) {
-      await Promise.all(
-        links
-          .filter((link) => !link.includes(host) && link.startsWith(protocol))
-          .map(async (link) => {
-            let linkHostname = urlParser.parse(link).hostname;
-            if (linkHostname?.startsWith("www.")) {
-              linkHostname = linkHostname.replace(/^www\./, "");
+      links.forEach(async (link) => {
+        if (!link.includes(host) && link.startsWith(protocol)) {
+          let linkHostname = urlParser.parse(link).hostname;
+          if (linkHostname?.startsWith("www.")) {
+            linkHostname = linkHostname.replace(/^www\./, "");
+          }
+          const validUrl = validDomain(linkHostname);
+          if (validUrl && !seenExternalLink[validUrl]) {
+            seenExternalLink[validUrl] = true;
+            try {
+              await supabase
+                .from("sitemap_external_link")
+                .insert({ url: validUrl });
+            } catch (error) {
+              console.log(error.message, "external links");
             }
-            const validUrl = validDomain(linkHostname);
-            if (validUrl) {
-              if (!seenExternalLink[validUrl]) {
-                seenExternalLink[validUrl] = true;
-                externalLink.push({ url: validUrl });
-              }
-              // if (!externalLink["url"]?.includes(validUrl)) {
-              //   ;
-              // }
-            }
-          })
-      );
-      const internalLinkSet = new Set(
-        links.filter((link) => link.includes(host) && !link.includes(ignore))
-      );
-
-      const internalLink = Array.from(internalLinkSet);
-
-      internalLink.filter((link) => {
-        return getUrl(link, host, protocol);
-      });
-
-      internalLink.forEach(async (link) => {
-        const endsWithignoreExtention = ignoreExtention.some((extension) =>
-          link.toLowerCase().endsWith(extension)
-        );
-        if (!endsWithignoreExtention) {
-          if (!pages?.includes(link)) {
-            internalLinks.push({ page_url: link });
-            pages.push(link);
-            await supabase
-              .from("crawling_sitemap_link")
-              .insert({ page_url: link });
-            // console.log(a);
-          } else {
-            return link;
           }
         }
       });
-      internalLink.forEach((link) => {
-        // console.log(link, "link");
-        // crawl({
-        //   url: link,
-        //   ignore,
-        // });
-      });
     }
-    return;
   } catch (error) {
     // Handle other errors
-    internalLinkError.push({
-      page_url: url,
-      message: `"Error:" ${error.message}`,
-    });
+    try {
+      await supabase.from("sitemap_internal_link").upsert(
+        { page_url: url, status: true, message: error.message },
+        {
+          onConflict: ["page_url"],
+          updateColumns: ["status", "message"],
+        }
+      );
+    } catch (error) {
+      console.log(error.message, "error internal link");
+    }
+
+    // console.log(response.error, "intenal page error supabase");
   }
 };
+let locCounter = 1;
+const locUrlExtractor = async (currentUrl) => {
+  let links = [];
+  try {
+    const response = await axios.get(currentUrl);
+    let $ = cheerio.load(response.data);
+    const locUrls = $("loc");
+    // console.log("Sitemap extract page No:", locCounter++);
+    for (const locUrl of locUrls) {
+      let link = $(locUrl).text();
+      if (link.includes(".xml")) {
+        const locLinks = await locUrlExtractor(link);
+        if (locLinks?.length > 0) {
+          links.push(...locLinks);
+          locLinks.map((link) => {
+            return { page_url: link };
+          });
+          await supabase
+            .from("sitemap_internal_link")
+            .upsert(locLinks, { onConflict: ["id"] });
+        }
+        for (const locLink of locLinks) {
+          links.push(locLink);
+        }
+      } else {
+        links.push(link);
+        await supabase.from("sitemap_internal_link").insert({ page_url: link });
+      }
+    }
+    return links;
+  } catch (error) {
+    console.log("locUrlExtractor Error :", error.message);
+  }
 
-if (true) {
-  // const url = `https://${process.argv[2]}/sitemap.xml`;
-  // const response = await
-  // const a = "https://patrika.com/sitemap-index.xml";
-  // const b = "https://patrika.com/sitemap.php";
-  // const c = "https://patrika.com/sitemap.xml.gz";
-  // const d = "https://patrika.com/sitemap.txt";
-  // const e = "https://patrika.com/sitemap/";
-  // const f = "https://patrika.com/sitemap/sitemap.xml";
-  // const g = "https://patrika.com/sitemapindex.xml";
-  // const h = "https://patrika.com/sitemap/indx.xml";
-  // const i = "https://patrika.com/sitemap/.xml";
-  // const j = "https://patrika.com/sitemap.xml";
-  // const urlArray = [a, b, c, d, e, f, g, h, i, j];
-  // urlArray.forEach(async (link) => {
-  //   try {
-  //     const header = await axios.get(link);
-  //     console.log(header.status, ">>>>>>>>>>>>>>>", link);
-  //   } catch (error) {
-  //     console.log(error.message, link);
-  //   }
-  // });
-  // extractSitemap(url);
-  //   setInterval(errorInternalLinkAdd, 10000);
-  //   setInterval(internalLinkAdd, 10000);
-  //   setInterval(externalLinkAdd, 10000);
-} else {
-  console.log("No url found.");
-}
+  return links;
+};
+const processWithPages = async () => {
+  let run = true;
+  console.log(run, "run");
+  let crawlCounter = 2;
+  while (run == true) {
+    try {
+      const { data, error } = await supabase
+        .from("sitemap_internal_link")
+        .select()
+        .eq("status", false)
+        .eq("message", "pending")
+        .order("created_at", { ascending: true })
+        .limit(10);
+      //   console.log(response);
+      if (data?.length > 0) {
+        for (const link of data) {
+          // console.log(link.page_url, "pageUrl");
 
+          await crawl({
+            url: link.page_url,
+            ignore: "/search",
+          });
+          console.log("crawl end for start", crawlCounter++);
+        }
+      } else if (data?.length == 0 && !error) {
+        console.log(response, "response");
+        run = false;
+      }
+    } catch (e) {
+      console.log(e.message, "get internal link");
+    }
+  }
+  console.log("end");
+};
 (async () => {
-  const url = "https://mediagarh.com/robots.txt";
-  const response = await axios.get(url);
-  const sitemapUrls = response.data
-    .match(/Sitemap:\s*(\S+)/g)
-    .map((match) => match.split(": ")[1]);
-  console.log(sitemapUrls, ">>>>>>>>");
+  if (process.argv.length > 2) {
+    let run = false;
+    const url = `https://${process.argv[2]}/robots.txt`;
+    const response = await axios.get(url);
+    const sitemapUrls = response.data
+      .match(/Sitemap:\s*(\S+)/g)
+      .map((match) => match.split(": ")[1]);
+    if (sitemapUrls?.length > 0) {
+      for (const sitemapUrl of sitemapUrls) {
+        locUrlExtractor(sitemapUrl);
+        run = true;
+      }
+      setTimeout(processWithPages, 10000);
+    } else {
+      console.log("No sitemap found");
+    }
+  } else {
+    console.log("no url found");
+  }
 })();
