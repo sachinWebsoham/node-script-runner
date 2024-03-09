f6b1c3591ad5f393ebc59b1ddff30784f8115d8f
sachinWebsoham <142967198+sachinWebsoham@users.noreply.github.com>
2024-03-09 18:25:06 +0530

    Update puppeteer_example.js

--- src/puppeteer_example.js
+++ src/puppeteer_example.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.example.com');
+  await page.goto('https://www.nytimes.com');
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
