90944e0a201aa521b080cb205fadfa7e1113ea7d
sachinWebsoham <142967198+sachinWebsoham@users.noreply.github.com>
2024-03-19 23:39:54 +0530

    update

--- config/config.js
+++ config/config.js
@@ -6,4 +6,8 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6b2x6a3Zvd2xmc2FoZG5ubHpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2MTQ5OTYsImV4cCI6MjAyNTE5MDk5Nn0.tqFQNFCi12D-hB4CS7DSDCDCQ9kWvNmh1kOfA90LZ0I";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-module.exports = { supabase };
+const websohambaseUrl = "http://64.227.181.80:8000";
+const websohambaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzEwNzg2NjAwLAogICJleHAiOiAxODY4NTUzMDAwCn0.7dFRAPts3fsl1BDBGrTvcz95RI-Iw-DVxk_imD2hW-8";
+const websohambase = createClient(websohambaseUrl, websohambaseKey);
+module.exports = { supabase, websohambase };
